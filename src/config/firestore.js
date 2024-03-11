1e0278a62adcb12aec4b006feb67277934a19482
jayraytalla <jayraytallaworkacc@gmail.com>
2024-03-12 07:16:46 +0800

    Updated

--- src/config/firestore.js
+++ src/config/firestore.js
@@ -10,12 +10,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBn5DKS5C71JtqpPdoqJic0myhsfnbMG8Y",
-  authDomain: "chat-app-68dd9.firebaseapp.com",
-  projectId: "chat-app-68dd9",
-  storageBucket: "chat-app-68dd9.appspot.com",
-  messagingSenderId: "413694711496",
-  appId: "1:413694711496:web:e950e8f10d13a47488f7fd"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
